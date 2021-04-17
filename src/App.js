5166c68f1ebcaf6ddda8bd517a8d153055cdff36
shoneriki <ikirenohs@gmail.com>
2021-04-17 20:49:44 +0900

    beginning jsx in App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import TodoList from './TodoList'
 
 function App() {
   return (
-    <TodoList />
+    <>
+      <TodoList />
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Completed Todos</button>
+      <div>0 left to do</div>
+    </>
   )
 }
 
