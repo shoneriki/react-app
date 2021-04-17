6fcc41855c62c6967f40ca6a8de29a43f3534c94
shoneriki <ikirenohs@gmail.com>
2021-04-17 22:22:42 +0900

    toggle check, updated even with refresh

--- src/App.js
+++ src/App.js
@@ -17,6 +17,13 @@ function App() {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
   }, [todos])
 
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
     if (name === '') return
@@ -27,7 +34,7 @@ function App() {
   }
   return (
     <>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input ref={todoNameRef} type="text" />
       <button onClick={handleAddTodo}>Add Todo</button>
       <button>Clear Completed Todos</button>
