d9658303608db43529fc9a1422f32a6c446f42d9
shoneriki <ikirenohs@gmail.com>
2021-04-17 22:05:06 +0900

    fixed issue with todo name when adding todo with button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React, { useState } from 'react';
-import TodoList from './TodoList'
+import React, { useState, useRef } from 'react';
+import TodoList from './TodoList';
+import nextId from "react-id-generator"
+
 
 
 function App() {
-  const [todos, setTodos] = useState([{ id: 1, name: 'Todo 1', complete: true }])
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: nextId(), name: name, complete: false}]
+    })
+      todoNameRef.current.value = null
+  }
   return (
     <>
       <TodoList todos={todos} />
-      <input type="text" />
-      <button>Add Todo</button>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
       <button>Clear Completed Todos</button>
       <div>0 left to do</div>
     </>
